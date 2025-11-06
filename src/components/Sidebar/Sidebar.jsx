13da12f92063211b3092fe520b6a554c9b0e47c3
Afia Mubashshara Tamanna <afiatamanna00@gmail.com>
2025-11-06 22:03:05 +0600

    bottom sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -19,6 +19,22 @@ function Sidebar() {
                 </div>
             </div>
         </div>
+
+
+        <div className="bottom">
+            <div className="bottom-item recent-entry">
+                <img src={assets.question_icon} alt="" />
+                <p>Help</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.history_icon} alt="" />
+                <p>Activity</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.setting_icon} alt="" />
+                <p>Settings</p>
+            </div>
+        </div>
         
     </div>
   )
