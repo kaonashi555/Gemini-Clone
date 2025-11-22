b6d8bcf766cb926a0ee736f46df7661f7ae10cb6
Afia Mubashshara Tamanna <afiatamanna00@gmail.com>
2025-11-22 22:51:48 +0600

    add main component: nav

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import './Main.css'
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img className='dp' src="src\assets\user_icon.jpeg" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Main
