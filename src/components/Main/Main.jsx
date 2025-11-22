37fd662faa171645f8a9b373cbc06c1f76eb3be9
Afia Mubashshara Tamanna <afiatamanna00@gmail.com>
2025-11-22 23:47:48 +0600

    add main_container:greet,cards

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,35 @@
 import './Main.css'
-
+import {assets} from '../../assets/assets'
 const Main = () => {
   return (
     <div className="main">
       <div className="nav">
         <p>Gemini</p>
         <img className='dp' src="src\assets\user_icon.jpeg" alt="" />
+      </div>
+      <div className="main_container">
+        <div className="greet">
+          <p><span>Hello, Afia.</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: urban planning</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
       </div>
     </div>
   )
